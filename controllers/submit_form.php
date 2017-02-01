3ba91468c2e2f86930e5f3a921423308d4bd60fe
Juliane Blier <blier.juliane@gmail.com>
2017-02-01 14:43:01 +0100

    Vérification si champ vide

--- controllers/submit_form.php
+++ controllers/submit_form.php
@@ -1,8 +1,12 @@
 <?php
-  require_once './../vendor/autoload.php';
-  require_once './../Model/AddPost.php';
-  require_once './../Model/Database.php';
+  if (!$_POST['title'] || !$_POST['author'] || !$_POST['content']) {
+    header('Location: ../index.php?p=form');
+  } else {
+    require_once './../vendor/autoload.php';
+    require_once './../Model/AddPost.php';
+    require_once './../Model/Database.php';
 
-  (new AddPost())->savePost();
+    (new AddPost())->savePost();
 
-  header('Location: ../index.php');
+    header('Location: ../index.php');
+  }
