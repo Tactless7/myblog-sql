83ac659684b91476097ce50e432044875f4e1d2a
Juliane Blier <blier.juliane@gmail.com>
2017-02-01 16:39:25 +0100

    Organisation du controller Ajout de la possibilité de l'édition

--- controllers/submit_form.php
+++ controllers/submit_form.php
@@ -1,10 +1,11 @@
 <?php
   session_start();
+  $_SESSION['error'] = false;
 
-  if (!$_POST['title'] || !$_POST['author'] || !$_POST['content']) {
+  if ( $_SERVER['REQUEST_METHOD'] === 'POST' && (!$_POST['title'] || !$_POST['author'] || !$_POST['content'])) {
     $_SESSION['error'] = true;
     header('Location: ../index.php?p=form');
-  } elseif($_POST['title'] && $_POST['author'] && $_POST['content']){
+  } elseif($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['title'] && $_POST['author'] && $_POST['content']){
     require_once './../vendor/autoload.php';
     require_once './../Model/AddPost.php';
     require_once './../Model/Database.php';
@@ -16,6 +17,6 @@
   }
 
   if(isset($_GET['id'])){
-    //require form.php avec l'id du post
     //values remplies avec getters
+    require_once '../views/form.php'; //avec l'id du post
   }
